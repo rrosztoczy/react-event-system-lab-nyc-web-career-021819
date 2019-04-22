de07eeee2bb39e6558b6c12f140ef62c16c70262
rrosztoczy <rrosztoczy@gmail.com>
2019-04-22 17:43:48 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
+import React, { Component } from 'react';
 // Code EyesOnMe Component Here
+
+export default class EyesOnMe extends Component {
+
+  printGood = () => {
+    console.log("Good!")
+  }
+
+  printHey = () => {
+    console.log("Hey! Eyes on me!")
+  }
+
+  render () {
+    return (<button onFocus={this.printGood} onBlur={this.printHey}></button>)
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
+import React, { Component } from 'react';
 // Code Keypad Component Here
+
+export default class Keypad extends Component {
+
+  printPW = () => {
+    console.log('Entering password...')
+  }
+  render () {
+    return <input type="password" onKeyUp={this.printPW}></input>
+  }
+}
